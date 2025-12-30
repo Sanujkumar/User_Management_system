01dbc0f9b8527263ca44e0a11773c769a5088204
sanuj <sanujkumar67943@gmail.com>
2025-12-31 01:14:47 +0530

    add cors in frontend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,16 +12,20 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://user-management-system-kohl-three.vercel.app"
+    ],
   })
 );
+  
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
-  res.send("hi sanuj , welcome back to my home");
+    res.send("hi sanuj , welcome back to my home");
 });
 
 export default app; // 🔥 MOST IMPORTANT
